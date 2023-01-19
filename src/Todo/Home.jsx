1e5f3338f6b99f22373a052745f143bfa51f3164
vicantlop <105176088+vicantlop@users.noreply.github.com>
2023-01-19 14:30:37 -0800

    created custom useFetch hook

--- src/Todo/Home.jsx
+++ src/Todo/Home.jsx
@@ -1,31 +1,15 @@
-import { useState, useEffect } from "react";
-import BlogList from "./BlogList"
+import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
-    { title: "Welcome party!", body: "lorem ipsum...", author: "yoshi", id: 2 },
-    {
-      title: "Web dev top tips",
-      body: "lorem ipsum...",
-      author: "mario",
-      id: 3,
-    },
-  ]);
-  const [name, setName] = useState('mario')
 
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter((blog) => blog.id !== id)
-    setBlogs(newBlogs)
-  }
-
-  useEffect(() => {
-    console.log('use Effect ran')
-  }, [])
+  const { data: blogs, isPending, error } = useFetch("http://localhost:8000/blogs")
 
   return (
     <div className="home">
-      {blogs && <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete}/>}
+      { error && <div>{error}</div>}
+      { isPending && <div>Loading...</div>}
+      {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
     </div>
   );
 };
