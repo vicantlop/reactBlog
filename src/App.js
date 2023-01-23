f902a25f5290e6aff580d46893adaf46625edbf8
vicantlop <105176088+vicantlop@users.noreply.github.com>
2023-01-22 18:00:03 -0800

    created blog details page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import Todo from './Todo/Todo'
+import Blog  from './Blog/Blog'
 import { BrowserRouter as Router} from 'react-router-dom';
 
 function App() {
   return (
     <Router>
-      <Todo />
+      <Blog />
     </Router>
   );
 }
--- src/Blog/CreateBlog.jsx
+++ src/Blog/CreateBlog.jsx
@@ -2,10 +2,6 @@ const CreateBlog = () => {
   return (
     <div className="create">
       <h2>Add a new Blog</h2>
-      <form action="" method="post">
-        <label htmlFor="Name"></label>
-        <input type="text" className="Name"/>
-      </form>
     </div>
   );
 }
