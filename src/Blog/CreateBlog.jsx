73ecf12f877a9a407b01a8008e8ca38d364d0eed
vicantlop <105176088+vicantlop@users.noreply.github.com>
2023-01-22 18:52:04 -0800

    can successfully add blog

--- src/Blog/CreateBlog.jsx
+++ src/Blog/CreateBlog.jsx
@@ -1,9 +1,43 @@
+import { useState } from "react";
+
 const CreateBlog = () => {
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [author, setAuthor] = useState("mario");
+  const [isPending, setIsPending] = useState(false)
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const blog = { title, body, author };
+
+    setIsPending(true)
+
+    fetch('http://localhost:8000/blogs', {
+      method: 'POST',
+      headers: { "Content-Type": "application/json"},
+      body: JSON.stringify(blog)
+    }).then(() => {
+      setIsPending(false)
+    })
+  }
+
   return (
     <div className="create">
-      <h2>Add a new Blog</h2>
+      <h2>Add a New Blog</h2>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="">Blog title:</label>
+        <input type="text" required value={title} onChange={(e) => setTitle(e.target.value)}/>
+        <label htmlFor="">Blog title:</label>
+        <textarea required value={body} onChange={(e) => setBody(e.target.value)}></textarea>
+        <label htmlFor="">Blog author:</label>
+        <select onChange={(e) => setAuthor(e.target.value)}>
+          <option value="mario">mario</option>
+          <option value="yoshi">yoshi</option>
+        </select>
+        {isPending ? <button disabled>adding blog...</button> : <button>Add Blog</button>}
+      </form>
     </div>
   );
-}
- 
+};
+
 export default CreateBlog;
