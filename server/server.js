7b4cdc7ba0e95c673ff72d63807182317947b4ac
vicantlop <105176088+vicantlop@users.noreply.github.com>
2023-01-25 10:08:22 -0800

    created user schema

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ const dotenv = require("dotenv");
 const path = require('path')
 dotenv.config({path: path.resolve(__dirname, './.env')});
 
+const User = require('./user')
+
 const express = require('express')
 const morgan = require('morgan')
 const mongoose = require('mongoose')
@@ -22,3 +24,18 @@ mongoose.connect(process.env.MONGO_URI)
   .catch((error) => {
     console.log(error)
   })
+
+  //routes
+
+app.post("/", async (req, res, next) => {
+  const {firstName, lastName, username, password, email} = req.body
+  try {
+    const user = await User.create({firstName, lastName, username, password, email})
+    res.status(200).json(user)
+  } catch(error) {
+    res.status(400).json({error: error.message})
+    console.log(error)
+  }
+
+  res.json({mssg: "POST a new user"})
+})
--- server/user.js
+++ server/user.js
@@ -1,18 +1,31 @@
-const { MongoClient } = require('mongodb')
+const mongoose = require("mongoose");
 
-let dbConnection
+const Schema = mongoose.Schema;
 
-module.exports = {
-  connectToDb: (cb) => {
-    MongoClient.connect('mongodb://localhost:27017/bookstore')
-      .then(client => {
-        dbConnection = client.db()
-        return cb()
-      })
-      .catch(err => {
-        console.log(err)
-        return cb(err)
-      })
+const userSchema = new Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
   },
-  getDb: () => dbConnection
-}
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("User", userSchema);
